ae27c19b2f19190b56ff095b9cc16ea4774b885e
Nolan Butcher <nolan.butcher@mlb.com>
2019-04-18 19:10:11 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Layout from "./Layout";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        if (process.env.NODE_ENV !== "production") {
+            const { whyDidYouUpdate } = require("why-did-you-update");
+            whyDidYouUpdate(React);
+        }
+
+        this.state = {
+            count: 0
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Layout />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { mount, shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import sinon from "sinon";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/* it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+}); */
+it("renders without crashing", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toBeTruthy();
 });
